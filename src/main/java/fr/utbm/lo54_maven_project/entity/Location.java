a9d74eff94b9a1258fd46c4836a79a3a21c99783
science162 <albertomouaffo@gmail.com>
2018-10-20 11:45:30 +0200

    creating location class

--- src/main/java/fr/utbm/lo54_maven_project/entity/Location.java
+++ src/main/java/fr/utbm/lo54_maven_project/entity/Location.java
@@ -5,10 +5,74 @@
  */
 package fr.utbm.lo54_maven_project.entity;
 
+import java.util.Objects;
+
 /**
  *
  * @author albert-einst
  */
-public class Location {
+public class Location implements java.io.Serializable {
+    
+    private int id;
+    private String city;
+
+    public Location(int id, String city) {
+        this.id = id;
+        this.city = city;
+    }
+
+    public Location() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "id=" + id + ", city=" + city + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 73 * hash + this.id;
+        hash = 73 * hash + Objects.hashCode(this.city);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
