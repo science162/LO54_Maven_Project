2cecbb18f20c319719f929915876a4025d1fadc9
science162 <albertomouaffo@gmail.com>
2018-10-20 11:22:40 +0200

    location_class

---
+++ src/main/java/fr/utbm/lo54_maven_project/entity/Location.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.utbm.lo54_maven_project.entity;
+
+/**
+ *
+ * @author albert-einst
+ */
+public class Location {
+    
+}
