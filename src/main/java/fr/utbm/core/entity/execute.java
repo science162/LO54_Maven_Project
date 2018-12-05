df625222418d13ba4977ca9130e862420cf97917
science162 <albertomouaffo@gmail.com>
2018-12-05 01:40:00 +0100

    insert_date

--- src/main/java/fr/utbm/core/entity/execute.java
+++ src/main/java/fr/utbm/core/entity/execute.java
@@ -5,6 +5,7 @@
  */
 package fr.utbm.core.entity;
 
+import fr.utbm.core.util.HibernateUtil;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,7 +17,7 @@ public class execute {
     public static void main(String[] argv){
 //     ClientDao cdao = new ClientDao();
 //        System.out.println(cdao.getClientById(1));
-    //HibernateUtil hu = new HibernateUtil();
+    HibernateUtil hu = new HibernateUtil();
     Date d = new Date();
     System.out.println(new SimpleDateFormat("dd-MM-yy").format(d));
     }
